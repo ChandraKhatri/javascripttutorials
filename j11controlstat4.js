37b43ed233d6d1ddf7030dab5db358b9664f05d5
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-02 06:57:10 +0545

    changed

--- j11controlstat4.js
+++ j11controlstat4.js
@@ -19,3 +19,11 @@
 //else{
 //    //code to be executed
 //}
+//Example:
+//var age = 18;
+//if(age>18){
+//    console.log("You are eligible to vote");
+//}
+//else{
+//    console.log("You are not eligible to vote");
+//}
---
+++ j12controlstat5.js
@@ -0,0 +1,17 @@
+// switch statement
+// switch statement is used to execute one block of code from multiple conditions. It is like if else if else if else if ladder. It is a better way to compare the value of a variable with multiple conditions.
+
+// Syntax:
+// switch(expression){
+//     case value1:
+//         //code to be executed;
+//         break;   
+//     case value2:
+//         //code to be executed;
+//         break;
+//     case value3:
+//         //code to be executed;
+//         break;
+//     default:
+//         //code to be executed;
+// }
