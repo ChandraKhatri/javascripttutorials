4c86af9e286dfea91c64de45560da87f0a995878
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-02 06:54:09 +0545

    changed

---
+++ j11controlstat4.js
@@ -0,0 +1,21 @@
+// control statement
+// if else
+// if else if else
+// switch case
+// for loop
+// while loop
+// do while loop
+// break
+// continue
+// return
+// throw
+// try catch finally
+//if else theory and example
+//if else statement is the most simple decision making statement. It is used to decide whether a certain statement or block of statements will be executed or not i.e if a certain condition is true then a block of statement is executed otherwise not.
+//Syntax:
+//if(condition){
+//    //code to be executed
+//}
+//else{
+//    //code to be executed
+//}
