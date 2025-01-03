39fcdda0fc6bad3eb832f4fed5238868aa4dbc9e
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-03 22:44:49 +0545

    changed

--- j7controlstat.js
+++ j7controlstat.js
@@ -1,13 +1,28 @@
 //start after long break
 
-//control flow (if)
-const isUserLoggedIn = true;
-if (isUserLoggedIn) {
-    console.log("You are logged in");
-}
+// control flow (if)
+// const isUserLoggedIn = true;
+// if (isUserLoggedIn) {
+//     console.log("You are logged in");
+// }
 // if (2 == "2") {
 //     console.log("This is true");
 // }
 // if (2 === "2"){
 //     console.log("This is false");
 // }
+// const temperature = "20";
+// if (temperature === 20) {
+//     console.log("If stat run");
+// }
+// else if (temperature === "20") {
+//     console.log("if else stat runs");
+// }
+// else {
+//         return 0;
+// }
+
+//short hand notation
+const isUserLoggedIn = true;
+ if (isUserLoggedIn ) console.log("You are logged in");
+isUserLoggedIn ? console.log("You are logged in again") : console.log("You are not logged in again and again");
