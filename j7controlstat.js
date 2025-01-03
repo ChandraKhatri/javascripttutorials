1c68d580eaacd7060930ce54d16968840b288730
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-03 22:27:11 +0545

    changed

--- j7controlstat.js
+++ j7controlstat.js
@@ -5,3 +5,9 @@ const isUserLoggedIn = true;
 if (isUserLoggedIn) {
     console.log("You are logged in");
 }
+// if (2 == "2") {
+//     console.log("This is true");
+// }
+// if (2 === "2"){
+//     console.log("This is false");
+// }
