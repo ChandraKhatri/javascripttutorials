237f6591d892f9586745bd68a42d484d3f2207b1
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-03 23:20:31 +0545

    changed

--- j9controlstat2.js
+++ j9controlstat2.js
@@ -1,2 +1,13 @@
-// code
-// 
+// truthy and falshy value
+// falsy value: 0, "", undefined, null, NaN, false
+// truthy value: all other values also array with no element i.e null element
+ 
+//Nullish coalescing operator
+let var1 ;
+// val1 = 5 ?? 10;// 5
+val1 = undefined ?? 10
+console.log(val1);
+
+//ternary 
+const teaPrice = 500;
+teaPrice > 500 ? console.log("I will buy") : console.log("I will not buy");
