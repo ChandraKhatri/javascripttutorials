4e349cd5a0ef44b4e43df7afd4ee7b03980005aa
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-03 22:09:23 +0545

    changed

--- j11controlstat4.js
+++ j11controlstat4.js
@@ -13,7 +13,7 @@
 //if else theory and example
 //if else statement is the most simple decision making statement. It is used to decide whether a certain statement or block of statements will be executed or not i.e if a certain condition is true then a block of statement is executed otherwise not.
 //Syntax:
-//if(condition){
+//if(condition){  
 //    //code to be executed
 //}
 //else{
