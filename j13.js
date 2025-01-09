917623a0a9225a629356989bfe96f91e6b0dae52
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-09 20:43:59 +0545

    add

--- j13.js
+++ j13.js
@@ -4,4 +4,4 @@
 // Date: 2025-01-09
 // Description: This is a javascript file
 // that will be used to learn javascript from scratch
-// and will be used to learn the basic concepts of javascript
+// and will be used to learn the basic concepts of javascrip
