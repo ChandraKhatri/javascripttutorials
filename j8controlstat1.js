facbde18778007111b02ce7506b95d29652b619f
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-11 21:58:40 +0545

    add

--- j8controlstat1.js
+++ j8controlstat1.js
@@ -1,7 +1,7 @@
 // switch statement
 // The switch statement is used to perform different actions based on different conditions.
 // Use the switch statement to select one of many code blocks to be executed.
-let month = "3";
+let month = 2;
 switch(month){
     case 1:
     console.log("january");
