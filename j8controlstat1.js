fb42c166099d5cfc0623664d4494fb1eb7b6add5
Chandra Khatri <chandrakhatri0987@gmail.com>
2025-01-11 22:00:10 +0545

    add

--- j8controlstat1.js
+++ j8controlstat1.js
@@ -12,7 +12,7 @@ switch(month){
     case 3:
     console.log("March");
     case 4:
-    console.log("March");
+    console.log("February");
     break;
     default:
         console.log("error");
